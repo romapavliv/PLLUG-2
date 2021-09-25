d895e1ac11e61c714a66aa21e035d32679e91dc4
WHAAAAAAAAAAT <shengkeru@gmail.com>
2021-09-25 23:23:06 +0300

    Completed first homework tasks.

---
+++ homework/CrashScript.js
@@ -0,0 +1,4 @@
+let a = Number.MAX_SAFE_INTEGER;
+while(1) {
+    a = a ** Number.MAX_SAFE_INTEGER;
+}
---
+++ homework/UpperToLower.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(`Enter your text: `, (text) => {
+    for (let i = 0; i < text.length; i++)
+        text = text[i] === text[i].toUpperCase() ? 
+                            text.substring(0, i) + text[i].toLowerCase() + text.substring(i + 1) :
+                            text.substring(0, i) + text[i].toUpperCase() + text.substring(i + 1);
+    console.log(`Your text after processing: ${text}`);
+    rl.close();
+});
