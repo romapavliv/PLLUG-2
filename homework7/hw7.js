a9e3308f59f6c46b7be88d53dc7dd9d17af3ff45
WHAAAAAAAAAAT <shengkeru@gmail.com>
2021-10-18 18:16:39 +0300

    Added tests that check iterations speed.

--- homework7/hw7.js
+++ homework7/hw7.js
@@ -2,7 +2,7 @@
 let obj = {};
 let objWithoutPrototype = Object.create(null);
 let map = new Map();
-const length = 100; // 10 000, 10 000 000
+const length = 10000000; // 10 000, 10 000 000
 
 function fillObjects(object, number) {
     for (let i = 0; i < number; ++i)
@@ -20,61 +20,96 @@ fillObjects(objWithoutPrototype, length);
 fillMap(map, length);
 
 // add a new key
-console.time('time spent on adding a new key to an ordinary object: ');
+console.time('time spent on adding a new key to an ordinary object');
 obj['key'] = 1;
-console.timeEnd('time spent on adding a new key to an ordinary object: '); // output: 100 keys - 0.07ms; 10 000 keys - 0.072ms; 10 000 000 keys - 0.089ms
+console.timeEnd('time spent on adding a new key to an ordinary object'); // output: 100 keys - 0.07ms; 10 000 keys - 0.072ms; 10 000 000 keys - 0.089ms
 
-console.time('time spent on adding a new key to an object without prototype: ');
+console.time('time spent on adding a new key to an object without prototype');
 objWithoutPrototype['key'] = 1;
-console.timeEnd('time spent on adding a new key to an object without prototype: '); // output: 100 keys - 0.004ms; 10 000 keys - 0.004ms; 10 000 000 keys - 0.005ms 
+console.timeEnd('time spent on adding a new key to an object without prototype'); // output: 100 keys - 0.004ms; 10 000 keys - 0.004ms; 10 000 000 keys - 0.005ms 
 
-console.time('time spent on adding a new key to a map: ');
+console.time('time spent on adding a new key to a map');
 map.set('key', 1);
-console.timeEnd('time spent on adding a new key to a map: '); //output: 100 keys - 0.004ms; 10 000 keys - 0.004ms; 10 000 000 keys - 0.006ms 
+console.timeEnd('time spent on adding a new key to a map'); //output: 100 keys - 0.004ms; 10 000 keys - 0.004ms; 10 000 000 keys - 0.006ms 
 console.log('');
 
 // get an element by key
 let element;
-console.time('time spent on getting an element by key from an ordinary object: ');
+console.time('time spent on getting an element by key from an ordinary object');
 element = obj['key'];
-console.timeEnd('time spent on getting an element by key from an ordinary object: '); // output: 100 keys - 0.004ms; 10 000 keys - 0.004ms; 10 000 000 keys - 0.004ms 
+console.timeEnd('time spent on getting an element by key from an ordinary object'); // output: 100 keys - 0.004ms; 10 000 keys - 0.004ms; 10 000 000 keys - 0.004ms 
 
-console.time('time spent on getting an element by key from an object without prototype: ');
+console.time('time spent on getting an element by key from an object without prototype');
 element = objWithoutPrototype['key'];
-console.timeEnd('time spent on getting an element by key from an object without prototype: '); // output: 100 keys - 0.005ms; 10 000 keys - 0.005ms; 10 000 000 keys - 0.005ms 
+console.timeEnd('time spent on getting an element by key from an object without prototype'); // output: 100 keys - 0.005ms; 10 000 keys - 0.005ms; 10 000 000 keys - 0.005ms 
 
-console.time('time spent on getting an element by key from a map: ');
+console.time('time spent on getting an element by key from a map');
 element = map.get('key');
-console.timeEnd('time spent on getting an element by key from a map: '); //output: 100 keys - 0.01ms; 10 000 keys - 0.009ms; 10 000 000 keys - 0.007ms 
+console.timeEnd('time spent on getting an element by key from a map'); //output: 100 keys - 0.01ms; 10 000 keys - 0.009ms; 10 000 000 keys - 0.007ms 
 console.log('');
 
 // delete a key 
-console.time('time spent on deleting a key from an ordinary object: ');
+console.time('time spent on deleting a key from an ordinary object');
 delete obj['key'];
-console.timeEnd('time spent on deleting a key from an ordinary object: '); // output: 100 keys - 0.006ms; 10 000 keys - 0.009ms; 10 000 000 keys - 0.006ms 
+console.timeEnd('time spent on deleting a key from an ordinary object'); // output: 100 keys - 0.006ms; 10 000 keys - 0.009ms; 10 000 000 keys - 0.006ms 
 
-console.time('time spent on deleting a key from an object without prototype: ');
+console.time('time spent on deleting a key from an object without prototype');
 delete objWithoutPrototype['key'];
-console.timeEnd('time spent on deleting a key from an object without prototype: '); // output: 100 keys - 0.003ms; 10 000 keys - 0.003ms; 10 000 000 keys - 0.018ms 
+console.timeEnd('time spent on deleting a key from an object without prototype'); // output: 100 keys - 0.003ms; 10 000 keys - 0.003ms; 10 000 000 keys - 0.018ms 
 
-console.time('time spent on deleting a key from a map: ');
+console.time('time spent on deleting a key from a map');
 map.delete('key');
-console.timeEnd('time spent on deleting a key from a map: '); //output: 100 keys - 0.004ms; 10 000 keys - 0.004ms; 10 000 000 keys - 0.004ms 
+console.timeEnd('time spent on deleting a key from a map'); //output: 100 keys - 0.004ms; 10 000 keys - 0.004ms; 10 000 000 keys - 0.004ms 
 console.log('');
 
 // convert to array
 let array;
-console.time('time spent on converting an ordinary object to array: ');
+console.time('time spent on converting an ordinary object to array');
 array = Object.entries(obj);
-console.timeEnd('time spent on converting an ordinary object to array: '); // output: 100 keys - 0.028ms; 10 000 keys - 1.857ms; 10 000 000 keys - 7.843s 
+console.timeEnd('time spent on converting an ordinary object to array'); // output: 100 keys - 0.028ms; 10 000 keys - 1.857ms; 10 000 000 keys - 7.843s 
 
-console.time('time spent on converting an object without prototype to array: ');
+console.time('time spent on converting an object without prototype to array');
 array = Object.entries(objWithoutPrototype);
-console.timeEnd('time spent on converting an object without prototype to array: '); // output: 100 keys - 0.049ms; 10 000 keys - 2.639ms; 10 000 000 keys - 18.313s 
+console.timeEnd('time spent on converting an object without prototype to array'); // output: 100 keys - 0.049ms; 10 000 keys - 2.639ms; 10 000 000 keys - 18.313s 
 
-console.time('time spent on converting a map to array: ');
+console.time('time spent on converting a map to array');
 array = Array.from(map);
-console.timeEnd('time spent on converting a map to array: '); //output: 100 keys - 0.014ms; 10 000 keys - 2.061ms; 10 000 000 keys - 2.178s 
+console.timeEnd('time spent on converting a map to array'); //output: 100 keys - 0.014ms; 10 000 keys - 2.061ms; 10 000 000 keys - 2.178s 
+console.log('');
+
+// iteration speed
+// a) without conversion
+console.time('speed of looping through an ordinary object');
+for (const key in obj) {
+    element = key;
+}
+console.timeEnd('speed of looping through an ordinary object'); //output: 100 keys - 0.02ms; 10 000 keys - 1.617ms; 10 000 000 keys - 1.988s 
+
+console.time('speed of looping through an object without prototype');
+for (const key in objWithoutPrototype) {
+    element = key;
+}
+console.timeEnd('speed of looping through an object without prototype'); //output: 100 keys - 0.008ms; 10 000 keys - 0.316ms; 10 000 000 keys - 2.011s 
+
+console.time('speed of looping through a map');
+for (const key of map) {
+    element = key;
+}
+console.timeEnd('speed of looping through a map'); //output: 100 keys - 0.014ms; 10 000 keys - 0.531ms; 10 000 000 keys - 253.008ms 
+console.log('');
+
+// b) with conversion
+console.time('speed of looping through an ordinary object (converted to array)');
+Object.entries(obj).forEach(pair => element = pair[0]);
+console.timeEnd('speed of looping through an ordinary object (converted to array)'); //output: 100 keys - 0.073ms; 10 000 keys - 2.405ms; 10 000 000 keys - 9.899s 
+
+console.time('speed of looping through an object without prototype (converted to array)');
+Object.entries(objWithoutPrototype).forEach(pair => element = pair[0]);
+console.timeEnd('speed of looping through an object without prototype (converted to array)'); //output: 100 keys - 0.052ms; 10 000 keys - 2.994ms; 10 000 000 keys - 18.493s 
+
+console.time('speed of looping through a map (converted to array)');
+Array.from(map).forEach(pair => element = pair[0]);
+console.timeEnd('speed of looping through a map (converted to array)'); //output: 100 keys - 0.043ms; 10 000 keys - 0.925ms; 10 000 000 keys - 2.123s 
 
 
 // 2) Create a function that converts an object to a map
